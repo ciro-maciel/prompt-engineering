d368017002dbd2a2d348cfd42e15821b7a48f411
Ciro Cesar Maciel <maciel.ciro@icloud.com>
2025-05-17 08:11:55 -0300

    ci: configure GitHub Pages deployment workflow with Docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: "https://ciro-maciel.github.io",      // URL do seu GitHub Pages
+    baseUrl: "/prompt-engineering/",           // caminho base (proj. pages)
+    // ...outros campos...
+};
